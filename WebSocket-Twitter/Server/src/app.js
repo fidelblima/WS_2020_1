e9c1a3ac2d3e39cb441d9caba8f22aa2cbbdf383
fidel <cr.belga@gmail.com>
2020-05-29 18:38:41 -0300

    Trabalho N1

--- WebSocket-Twitter/Server/src/app.js
+++ WebSocket-Twitter/Server/src/app.js
@@ -6,10 +6,10 @@ io.on('connection', socket => {
     
     var Twitter = require('node-tweet-stream')
         socket = new Twitter({
-      consumer_key: 'XbeCmJBWgJvnGRazIH2mwLMdD', //Dados do seu Twitter Developer
-      consumer_secret: 'gwxhnl1aSFGfERaKr57El2Fz9sUsFD42c5E835Peg390fs64U9', //Dados do seu Twitter Developer
-      token: '155238701-MwmcRKNlHNEUEBRzU8SwucMlKc1sWKZgJjpyvXqP', //Dados do seu Twitter Developer
-      token_secret: 'LnQ3UL2AuZZq0sUTWhCHkesfUYBv09Mdi1TZs2N1thlkF' //Dados do seu Twitter Developer
+      consumer_key: '', //Dados do seu Twitter Developer
+      consumer_secret: '', //Dados do seu Twitter Developer
+      token: '', //Dados do seu Twitter Developer
+      token_secret: '' //Dados do seu Twitter Developer
     })
 
     socket.track('playstation 5'); //Palavra chave para fazer a busca 
